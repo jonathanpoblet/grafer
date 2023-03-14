da88cd74cba1805974f5e7b687dd83608402f14b
Jonathan Poblet <jonathanpoblet228@gmail.com>
2023-03-14 14:01:55 -0300

    created nutrition-tools full responsive

--- client/src/pages/NutritionTools/NutritionTools.jsx
+++ client/src/pages/NutritionTools/NutritionTools.jsx
@@ -1,3 +1,29 @@
+import NutritionToolsCard from "../../components/NutritionToolsCard/NutritionToolsCard";
+import AlimentaryPlan from "../../assets/plan-alimentario.png";
+import EBook from "../../assets/e-books.png";
+import NutritionToolsImg from "../../assets/nutrition-tools.png";
+import Recetary from "../../assets/recetary.png";
+
+import "./nutritionTools.css";
+
 export default function NutritionTools() {
-  return <div>NutritionTools</div>;
+  return (
+    <div className="nutrition">
+      <h1 className="nutrition-title">Herramientas de nutrición</h1>
+      <div className="nutrition-cards">
+        <NutritionToolsCard
+          img={AlimentaryPlan}
+          title="Planes alimentarios"
+          length="10"
+        />
+        <NutritionToolsCard img={EBook} title="E-books" length="6" />
+        <NutritionToolsCard
+          img={NutritionToolsImg}
+          title="Herramientas para nutricionistas"
+          length="8"
+        />
+        <NutritionToolsCard img={Recetary} title="Recetarios" length="15" />
+      </div>
+    </div>
+  );
 }
