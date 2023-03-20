7c70b2a8079816ac5de660645301158b2e3ce3de
Jonathan Poblet <jonathanpoblet228@gmail.com>
2023-03-20 17:34:19 -0300

    controller post products to database added

--- server/src/controllers/controllerProducts.js
+++ server/src/controllers/controllerProducts.js
@@ -1,10 +1,56 @@
 import { containerProducts } from "../container/containerProducts.js";
+import { randomUUID } from 'crypto';
 
 export async function controllerGetProducts(req, res) {
-  const products = await  containerProducts.getAll();
+  const products = await containerProducts.getAll();
   res.json(products);
 }
 
 export async function controllerPostProducts(req, res) {
+  const product = {
+    alimentaryPlans: [],
+    identificator: randomUUID()
+  }
+  await containerProducts.save(product)
   res.json({ succesfull: "Product addedd" });
+}
+
+export async function controllerGetProductCollection(req,res) {
+  const identificator = req.params.identificator;
+  const collection = await containerProducts.getById(identificator);
+  if(collection) {
+    res.json(collection);
+  } else {
+      res.status(404).json({error: `Collection with id ${identificator} not found`});
+  }
+}
+
+export async function controllerPutProductCollection(req, res) {
+  const identificator = req.params.identificator;
+  const collection = await containerProducts.getById(identificator);
+  if(collection) {
+    console.log(collection);
+    const product = req.body
+    if(collection["alimentaryPlans"]) {
+      collection.alimentaryPlans.push(product);
+      await containerProducts.changeById(identificator,collection);
+      res.json(collection);
+    } else if(collection["ebooks"]) {
+      collection.ebooks.push(product);
+      await containerProducts.changeById(identificator,collection);
+      res.json(collection);
+    } else if(collection["nutritionalTools"]) {
+      collection.nutritionalTools.push(product);
+      await containerProducts.changeById(identificator,collection);
+      res.json(collection);
+    } else if(collection["recetaries"]) {
+      collection.recetaries.push(product);
+      await containerProducts.changeById(identificator,collection);
+      res.json(collection);
+    } else {
+      res.json({error: "Invalid Type"})
+    }
+  } else {
+      res.status(404).json({error: `Collection with id ${identificator} not found`});
+  }
 }
